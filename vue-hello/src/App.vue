79ec07ad0085b76f54c0f27fc550dd26c995a7f1
baseon2016 <baseon_000@163.com>
2019-06-20 17:27:03 +0800

    6-20 update

---
+++ vue-hello/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>hello world</h1>
+    <Button/>
+  </div>
+</template>
+
+<script>
+import Button from "./components/Button";
+export default {
+  //小名  不能和html的标签一样 一般都是小写 一般是必须的
+  name: "app",
+  components: {
+    Button
+  }
+};
+</script>
+
+<style>
+</style>
