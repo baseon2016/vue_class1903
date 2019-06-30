9a24d52e12edc5ec79b3451c1a44889be9160ad3
baseon2016 <baseon_000@163.com>
2019-07-01 00:36:10 +0800

    vue-todoList周末作业

---
+++ vue-todo/src/App.vue
@@ -0,0 +1,165 @@
+<template>
+  <div class="todo">
+    <div>
+      <span class="completeAll" @click="completeAllToggle">全选</span>
+      <input
+        @keyup.enter="submit"
+        type="text"
+        v-model="todoVal"
+        placeholder="What needs to be done?"
+        class="todoInput"
+      >
+    </div>
+
+    <ul v-for="item in todoListR" :key="item.id">
+      <li
+        v-show="showItem==='all'?true:showItem==='complete'?item.complete===true:item.complete===false"
+        @mouseenter="item.showDel=true"
+        @mouseleave="item.showDel=false"
+      >
+        <span
+          :class="{select:true,selected:item.complete===true}"
+          @click="item.complete=!item.complete"
+        ></span>
+        <span class="content">{{item.content}}</span>
+        <span class="del" @click="del(item.id)" v-show="item.showDel">X</span>
+      </li>
+    </ul>
+    <Func
+      v-show="todoList.length!=0"
+      :todoNum="todoNum"
+      :showItem="showItem"
+      :showClear="showClear"
+      @all="all"
+      @active="active"
+      @complete="complete"
+      @clear="clear"
+    />
+  </div>
+</template>
+
+<script>
+import shortId from "shortid";
+import Func from "./components/Func";
+export default {
+  name: "app",
+  components: {
+    Func
+  },
+  data() {
+    return {
+      todoVal: "",
+      showItem: "all",
+      todoList: []
+    };
+  },
+  computed: {
+    todoListR() {
+      return [...this.todoList].reverse();
+    },
+    todoNum() {
+      const num = this.todoList.filter(element => element.complete === false);
+      return num.length;
+    },
+    showClear() {
+      return !this.todoList.every(element => element.complete === false);
+    }
+  },
+  methods: {
+    submit() {
+      if (this.todoVal.trim() != "") {
+        let newItem = {
+          id: shortId(),
+          complete: false,
+          content: this.todoVal,
+          showDel: false
+        };
+        this.todoList.push(newItem);
+        this.todoVal = "";
+        console.log(this.todoList);
+      } else {
+        alert("请输入有效内容");
+      }
+    },
+    completeAllToggle() {
+      if (this.todoList.every(element => element.complete === true)) {
+        this.todoList.forEach(element => (element.complete = false));
+      } else {
+        this.todoList.forEach(element => (element.complete = true));
+      }
+    },
+    del(id) {
+      this.todoList = this.todoList.filter(element => element.id != id);
+    },
+    all() {
+      this.showItem = "all";
+    },
+    complete() {
+      this.showItem = "complete";
+    },
+    active() {
+      this.showItem = "active";
+    },
+    clear() {
+      this.todoList = this.todoList.filter(
+        element => element.complete === false
+      );
+    }
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+body {
+  margin: 0;
+}
+a {
+  text-decoration: none;
+}
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+.todo {
+  width: 500px;
+  border: 1px solid #ababab;
+  margin: 0 auto;
+}
+.todo .todoInput {
+  outline: 0;
+  border: 0;
+  padding: 10px;
+}
+.todo li {
+  border-bottom: 1px solid #eee;
+  padding: 5px 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.todo span {
+  margin: 0 10px;
+}
+.todo .select {
+  display: block;
+  width: 15px;
+  height: 15px;
+  border-radius: 50%;
+  border: 1px solid #cdcdcd;
+}
+.todo .select.selected {
+  background-color: yellowgreen;
+}
+.todo .content {
+  flex-grow: 1;
+}
+.todo .completeAll,
+.todo .select,
+.todo .del {
+  cursor: pointer;
+}
+</style>
---
+++ vue-todo/src/components/Func.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="func">
+    <div>
+      <span>{{todoNum}} items left</span>
+    </div>
+    <div class="options">
+      <span :class="{selected:showItem==='all'?true:false}" @click="$emit('all')">All</span>
+      <span :class="{selected:showItem==='active'?true:false}" @click="$emit('active')">Active</span>
+      <span
+        :class="{selected:showItem==='complete'?true:false}"
+        @click="$emit('complete')"
+      >Completed</span>
+    </div>
+    <div class="clear">
+      <span v-show="showClear" @click="$emit('clear')">Clear completed</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "func",
+  props: {
+    todoNum: {
+      type: Number,
+      required: true
+    },
+    showItem: {
+      type: String,
+      required: true
+    },
+    showClear: {
+      type: Boolean,
+      required: true
+    }
+  }
+};
+</script>
+
+<style scoped>
+.func {
+  padding: 5px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.func .options {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.func .options span,
+.func .clear span {
+  cursor: pointer;
+}
+.func span {
+  font-size: 12px;
+}
+.func .options span {
+  border: 1px solid transparent;
+}
+.func .options span.selected {
+  border-color: hotpink;
+}
+.func .clear span:hover {
+  text-decoration: underline;
+}
+</style>
